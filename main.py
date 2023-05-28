79d86512afa20d619de4f5149a8ad5668611524b
beomtaek <beomtaegg@gmail.com>
2023-05-28 10:38:23 +0900

    finished user,event routes on planner.go to database connection

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
 
 import uvicorn
 
 # FastAPI() 인스턴스 생성후 앞서 정의한 라우트 등록
 app = FastAPI()
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 # uvicorn.run() 메서드 이용하여 8000 포트에서 애플리케이션 실행
 if __name__=="__main__":
